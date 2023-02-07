02b4acbb7f9597150820c8fbff28bb851d680b68
Tanvir <{ID}+{username}@users.noreply.github.com>
2023-02-06 18:31:18 -0800

    my updates for ZCU 104

--- README.md
+++ README.md
@@ -1,5 +1,170 @@
 # Rowhammer Tester
 
+# My notes for building a working example using a ZCU-104 board.
+
+This is a working setup for my experiments
+
+'''
+OS: NAME="Ubuntu"
+VERSION="20.04.3 LTS (Focal Fossa)"
+
+Xilinx tools: 
+Vivado v2021.2 (64-bit)
+SW Build 3367213 on Tue Oct 19 02:47:39 MDT 2021
+IP Build 3369179 on Thu Oct 21 08:25:16 MDT 2021
+
+Board: zcu104
+'''
+
+## Installation
+1. Installation:
+'''
+apt install git build-essential autoconf cmake flex bison libftdi-dev libjson-c-dev libevent-dev libtinfo-dev uml-utilities python3 python3-venv python3-wheel protobuf-compiler libcairo2 libftdi1-2 libftdi1-dev libhidapi-hidraw0 libhidapi-dev libudev-dev pkg-config tree zlib1g-dev zip unzip
+
+apt  install gcc-aarch64-linux-gnu // mandatory for building litex server properly
+'''
+
+2. Clone and make dependencies:
+'''
+git clone --recursive https://github.com/tanvirarafin/rowhammer-tester.git
+cd rowhammer-tester
+make deps
+'''
+
+3. Set up the paths
+'''
+source venv/bin/activate
+source /PATH/TO/Vivado/VERSION/settings64.sh
+'''
+
+4. Set up the variables
+'''
+export TARGET=zcu104
+export IP_ADDRESS=192.168.100.50  
+'''
+
+5. Now build the bit stream
+'''
+make build
+make  test
+'''
+
+The resulting bitstream file will be located in `build/zcu104/gateware/xilinx_zcu104.bit`. Rename it to zcu104.bit. 
+
+6. Now make the EtherBone 
+
+Make sure you are using gcc-aarch64-linux-gnu for building
+
+'''
+cd firmware/zcu104/etherbone/
+make LDFLAGS=-static
+'''
+
+Or you can use the `zcu104_etherbone` binary already present in the `firmware/zcu104/etherbone/build` folder
+
+7. Create the SD card:
+Start with the prebuilt image in the `prebuilt/` folder. Use the following to create the SD card. 
+
+'''
+sudo dd status=progress oflag=sync bs=4M if=zcu104.img of=<DEVICE>
+'''
+
+We will need to update the `zcu104.bit` and the  `zcu104_etherbone` later to make the image work as intended.
+
+8. Network setup
+Put in the SD card in ZCU104. To make the ZCU104 boot from the SD card it is necessary to ensure proper switch configuration. The mode switch (SW6) consisting of 4 switches is located near the FMC LPC Connector (J5) (the same side of the board as USB, HDMI, Ethernet). For details, see “ZCU104 Evaluation Board User Guide (UG1267)”. To use an SD card, configure the switches as follows:
+	1. ON
+	2. OFF
+	3. OFF
+	4. OFF
+
+Connect the board with an ethernet wire. 
+
+Now change your Ubuntu network setting manually (Go to Wired setting > IPV4) Select IPV4 as manual and set your address to `192.168.100.100` and netmask to `255.255.255.0`. Hit apply. Restart the network by hitting the toggle button on the wired setting.
+
+Turn on the SD card. It should start with a blinking pattern.
+
+First check if the network connection is okay by pinging to  the board. In the host machine type
+'''
+ping 192.168.100.50
+'''
+
+If everything went well so far you should be able to see ping response. If not check your network connection to make sure your IP address is set to `192.168.100.100`
+
+Now connect the micro-usb cable  with the board. Use picocom for communication
+
+'''
+picocom -b 115200 /dev/ttyUSB1
+'''  
+
+9. Change the bit file and EtherBone files:
+
+a. Ssh to the board
+'''
+ssh root@192.168.100.50
+'''
+b. Use scp to load the new bit-stream:
+'''
+scp build/zcu104/gateware/zcu104.bit root@192.168.100.50:/boot/zcu104.bit
+'''
+c. Kill EtherBone on the board:
+'''
+killall zcu104_etherbone
+'''
+d. Now use scp to replace the EtherBone file
+'''
+scp firmware/zcu104/etherbone/build/zcu104_etherbone  root@192.168.100.50:/bin/zcu104_etherbone 
+'''
+
+e. Reboot the board. If everything goes well you should see EtherBone OK in the boot process via the serial connection just before log in prompt.
+
+## Running the experiments:
+If your EtherBone server is running properly then proceed with the experiments.
+
+First try the leds.py example in the row hammer-tester/scipts folder.
+
+For this you will need to run three terminals
+```
+Terminal 1. Connect the serial prompt to see the experiments
+picocom -b 115200 /dev/ttyUSB1
+```
+Terminal 2. Inside the rowhammer-tester folder type the following
+```
+source venv/bin/activate
+export TARGET=zcu104
+export IP_ADDRESS=192.168.100.50  
+
+make srv
+```
+If everything works then you will see the following in terminal 2. There would not be any error on the output and it will wait as an open server.
+```
+litex_server --udp --udp-ip 192.168.100.50 --udp-port 1234
+[CommUDP] ip: 192.168.100.50 / port: 1234 / tcp port: 1234
+```
+Terminal 3.
+In this terminal you will run the experiment. Type the following:
+```
+source venv/bin/activate
+export TARGET=zcu104
+export IP_ADDRESS=192.168.100.50  
+cd rowhammer-tester/rowhammer_tester/scripts/
+python leds.py 
+```
+This will give the following output and the light pattern on the leds will change. 
+```
+Using generated target files in: ../../build/zcu104
+Board info: Row Hammer Tester SoC on xczu7ev-ffvc1156-2-i, git: 88f83152ee7306fc5288f87ad4a63efa2f772dc1 2023-02-06 16:25:38
+```
+You will also see something like `Connected with 127.0.0.1:41292` in terminal 2
+
+Further experiments
+In terminal 3 you can ctrl+C the leds.py experiment and run others. Such as
+```
+python hw_rowhammer.py --all-rows --nrows 5
+```
+You should not see any hangups rather a fast filling of the memory and then error counts etc.
+
+
 Copyright (c) 2020-2022 [Antmicro](https://www.antmicro.com)
 
 The aim of this project is to provide a platform for testing the [DRAM "Row Hammer" vulnerability](https://users.ece.cmu.edu/~yoonguk/papers/kim-isca14.pdf).
---
+++ firmware/zcu104/etherbone/build/main.d
@@ -0,0 +1,230 @@
+build/main.o: main.c /usr/aarch64-linux-gnu/include/stdc-predef.h \
+ /usr/aarch64-linux-gnu/include/stdio.h \
+ /usr/aarch64-linux-gnu/include/bits/libc-header-start.h \
+ /usr/aarch64-linux-gnu/include/features.h \
+ /usr/aarch64-linux-gnu/include/sys/cdefs.h \
+ /usr/aarch64-linux-gnu/include/bits/wordsize.h \
+ /usr/aarch64-linux-gnu/include/bits/long-double.h \
+ /usr/aarch64-linux-gnu/include/gnu/stubs.h \
+ /usr/aarch64-linux-gnu/include/gnu/stubs-lp64.h \
+ /usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stddef.h \
+ /usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdarg.h \
+ /usr/aarch64-linux-gnu/include/bits/types.h \
+ /usr/aarch64-linux-gnu/include/bits/timesize.h \
+ /usr/aarch64-linux-gnu/include/bits/typesizes.h \
+ /usr/aarch64-linux-gnu/include/bits/time64.h \
+ /usr/aarch64-linux-gnu/include/bits/types/__fpos_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/__mbstate_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/__fpos64_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/__FILE.h \
+ /usr/aarch64-linux-gnu/include/bits/types/FILE.h \
+ /usr/aarch64-linux-gnu/include/bits/types/struct_FILE.h \
+ /usr/aarch64-linux-gnu/include/bits/stdio_lim.h \
+ /usr/aarch64-linux-gnu/include/bits/sys_errlist.h \
+ /usr/aarch64-linux-gnu/include/bits/stdio.h \
+ /usr/aarch64-linux-gnu/include/bits/stdio2.h \
+ /usr/aarch64-linux-gnu/include/stdlib.h \
+ /usr/aarch64-linux-gnu/include/bits/waitflags.h \
+ /usr/aarch64-linux-gnu/include/bits/waitstatus.h \
+ /usr/aarch64-linux-gnu/include/bits/floatn.h \
+ /usr/aarch64-linux-gnu/include/bits/floatn-common.h \
+ /usr/aarch64-linux-gnu/include/sys/types.h \
+ /usr/aarch64-linux-gnu/include/bits/types/clock_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/clockid_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/time_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/timer_t.h \
+ /usr/aarch64-linux-gnu/include/bits/stdint-intn.h \
+ /usr/aarch64-linux-gnu/include/endian.h \
+ /usr/aarch64-linux-gnu/include/bits/endian.h \
+ /usr/aarch64-linux-gnu/include/bits/endianness.h \
+ /usr/aarch64-linux-gnu/include/bits/byteswap.h \
+ /usr/aarch64-linux-gnu/include/bits/uintn-identity.h \
+ /usr/aarch64-linux-gnu/include/sys/select.h \
+ /usr/aarch64-linux-gnu/include/bits/select.h \
+ /usr/aarch64-linux-gnu/include/bits/types/sigset_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/__sigset_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/struct_timeval.h \
+ /usr/aarch64-linux-gnu/include/bits/types/struct_timespec.h \
+ /usr/aarch64-linux-gnu/include/bits/select2.h \
+ /usr/aarch64-linux-gnu/include/bits/pthreadtypes.h \
+ /usr/aarch64-linux-gnu/include/bits/thread-shared-types.h \
+ /usr/aarch64-linux-gnu/include/bits/pthreadtypes-arch.h \
+ /usr/aarch64-linux-gnu/include/bits/struct_mutex.h \
+ /usr/aarch64-linux-gnu/include/bits/struct_rwlock.h \
+ /usr/aarch64-linux-gnu/include/alloca.h \
+ /usr/aarch64-linux-gnu/include/bits/stdlib-bsearch.h \
+ /usr/aarch64-linux-gnu/include/bits/stdlib-float.h \
+ /usr/aarch64-linux-gnu/include/bits/stdlib.h \
+ /usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdint.h \
+ /usr/aarch64-linux-gnu/include/stdint.h \
+ /usr/aarch64-linux-gnu/include/bits/wchar.h \
+ /usr/aarch64-linux-gnu/include/bits/stdint-uintn.h \
+ /usr/aarch64-linux-gnu/include/string.h \
+ /usr/aarch64-linux-gnu/include/bits/types/locale_t.h \
+ /usr/aarch64-linux-gnu/include/bits/types/__locale_t.h \
+ /usr/aarch64-linux-gnu/include/strings.h \
+ /usr/aarch64-linux-gnu/include/bits/strings_fortified.h \
+ /usr/aarch64-linux-gnu/include/bits/string_fortified.h pl_mmap.h \
+ /usr/aarch64-linux-gnu/include/sys/mman.h \
+ /usr/aarch64-linux-gnu/include/bits/mman.h \
+ /usr/aarch64-linux-gnu/include/bits/mman-map-flags-generic.h \
+ /usr/aarch64-linux-gnu/include/bits/mman-linux.h \
+ /usr/aarch64-linux-gnu/include/bits/mman-shared.h udp_server.h debug.h \
+ etherbone.h cmdline.h \
+ /usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdbool.h
+
+/usr/aarch64-linux-gnu/include/stdc-predef.h:
+
+/usr/aarch64-linux-gnu/include/stdio.h:
+
+/usr/aarch64-linux-gnu/include/bits/libc-header-start.h:
+
+/usr/aarch64-linux-gnu/include/features.h:
+
+/usr/aarch64-linux-gnu/include/sys/cdefs.h:
+
+/usr/aarch64-linux-gnu/include/bits/wordsize.h:
+
+/usr/aarch64-linux-gnu/include/bits/long-double.h:
+
+/usr/aarch64-linux-gnu/include/gnu/stubs.h:
+
+/usr/aarch64-linux-gnu/include/gnu/stubs-lp64.h:
+
+/usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stddef.h:
+
+/usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdarg.h:
+
+/usr/aarch64-linux-gnu/include/bits/types.h:
+
+/usr/aarch64-linux-gnu/include/bits/timesize.h:
+
+/usr/aarch64-linux-gnu/include/bits/typesizes.h:
+
+/usr/aarch64-linux-gnu/include/bits/time64.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/__fpos_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/__mbstate_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/__fpos64_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/__FILE.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/FILE.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/struct_FILE.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdio_lim.h:
+
+/usr/aarch64-linux-gnu/include/bits/sys_errlist.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdio.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdio2.h:
+
+/usr/aarch64-linux-gnu/include/stdlib.h:
+
+/usr/aarch64-linux-gnu/include/bits/waitflags.h:
+
+/usr/aarch64-linux-gnu/include/bits/waitstatus.h:
+
+/usr/aarch64-linux-gnu/include/bits/floatn.h:
+
+/usr/aarch64-linux-gnu/include/bits/floatn-common.h:
+
+/usr/aarch64-linux-gnu/include/sys/types.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/clock_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/clockid_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/time_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/timer_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdint-intn.h:
+
+/usr/aarch64-linux-gnu/include/endian.h:
+
+/usr/aarch64-linux-gnu/include/bits/endian.h:
+
+/usr/aarch64-linux-gnu/include/bits/endianness.h:
+
+/usr/aarch64-linux-gnu/include/bits/byteswap.h:
+
+/usr/aarch64-linux-gnu/include/bits/uintn-identity.h:
+
+/usr/aarch64-linux-gnu/include/sys/select.h:
+
+/usr/aarch64-linux-gnu/include/bits/select.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/sigset_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/__sigset_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/struct_timeval.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/struct_timespec.h:
+
+/usr/aarch64-linux-gnu/include/bits/select2.h:
+
+/usr/aarch64-linux-gnu/include/bits/pthreadtypes.h:
+
+/usr/aarch64-linux-gnu/include/bits/thread-shared-types.h:
+
+/usr/aarch64-linux-gnu/include/bits/pthreadtypes-arch.h:
+
+/usr/aarch64-linux-gnu/include/bits/struct_mutex.h:
+
+/usr/aarch64-linux-gnu/include/bits/struct_rwlock.h:
+
+/usr/aarch64-linux-gnu/include/alloca.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdlib-bsearch.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdlib-float.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdlib.h:
+
+/usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdint.h:
+
+/usr/aarch64-linux-gnu/include/stdint.h:
+
+/usr/aarch64-linux-gnu/include/bits/wchar.h:
+
+/usr/aarch64-linux-gnu/include/bits/stdint-uintn.h:
+
+/usr/aarch64-linux-gnu/include/string.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/locale_t.h:
+
+/usr/aarch64-linux-gnu/include/bits/types/__locale_t.h:
+
+/usr/aarch64-linux-gnu/include/strings.h:
+
+/usr/aarch64-linux-gnu/include/bits/strings_fortified.h:
+
+/usr/aarch64-linux-gnu/include/bits/string_fortified.h:
+
+pl_mmap.h:
+
+/usr/aarch64-linux-gnu/include/sys/mman.h:
+
+/usr/aarch64-linux-gnu/include/bits/mman.h:
+
+/usr/aarch64-linux-gnu/include/bits/mman-map-flags-generic.h:
+
+/usr/aarch64-linux-gnu/include/bits/mman-linux.h:
+
+/usr/aarch64-linux-gnu/include/bits/mman-shared.h:
+
+udp_server.h:
+
+debug.h:
+
+etherbone.h:
+
+cmdline.h:
+
+/usr/lib/gcc-cross/aarch64-linux-gnu/9/include/stdbool.h:
